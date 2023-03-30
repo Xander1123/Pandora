08a254f43e5dde42c99e116e914d5402075264a5
Xander1123 <recebovnunu@gmail.com>
2023-03-31 02:23:17 +0400

    deyisiklik olunan

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,14 @@ urlpatterns = [
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('',views.dashboard,name='dashboard'),
+
+
 
     path('activate/<uidb64>/<token>/',views.activate,name='activate'),
+    path('forgotPassword/',views.forgotPassword,name='forgotPassword'),
+    path('resetpassword_validate/<str:uidb64>/<str:token>/', views.resetpassword_validate, name='resetpassword_validate'),
+
 
 ]
